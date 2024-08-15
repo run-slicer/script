47138d80888ef0aaa5c3cc01f89bcc728d7f713c
Matouš Kučera <mk@kcra.me>
2024-08-15 13:19:06 +0200

    feat: options

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,66 @@
-export type EventType = "preload";
+export type OptionType = "group" | "button" | "checkbox" | "radio";
 
-export interface Event<T> {
-    type: EventType;
-    payload: T;
+export interface Option {
+    type: OptionType;
+    id: string;
+    label?: string;
 }
 
-export type EventListener<E extends Event<any>> = (event: E) => void | Promise<void>;
+export interface GroupOption extends Option {
+    type: "group";
+    options: Option[];
+}
 
-export interface PreloadEventPayload {
+export interface ButtonOption extends Option {
+    type: "button";
+}
+
+export interface CheckboxOption extends Option {
+    type: "checkbox";
+    checked: boolean;
+}
+
+export interface RadioItem {
+    id: string;
+    label?: string;
+}
+
+export interface RadioOption extends Option {
+    type: "radio";
+    items: RadioItem[];
+    selected: string; // id
+}
+
+export type EventType = "option_change" | "preload";
+
+export interface Event {
+    type: EventType;
+}
+
+export type EventListener<E extends Event> = (event: E, context: ScriptContext) => void | Promise<void>;
+
+export interface OptionChangeEvent extends Event {
+    type: "option_change";
+    option: Option;
+}
+
+export interface PreloadEvent extends Event {
+    type: "preload";
     data: Uint8Array;
 }
 
-export interface PreloadEvent extends Event<PreloadEventPayload> {
-    type: "preload";
-}
-
 interface EventMap {
+    option_change: OptionChangeEvent;
     preload: PreloadEvent;
 }
 
 export interface ScriptContext {
+    script: Script;
+    parent: ScriptContext | null;
+
     addEventListener<K extends EventType>(type: K, listener: EventListener<EventMap[K]>): void;
     removeEventListener<K extends EventType>(type: K, listener: EventListener<EventMap[K]>): void;
-    dispatchEvent<E extends Event<any>>(event: E): Promise<E>;
+    dispatchEvent<E extends Event>(event: E): Promise<E>;
 }
 
 export interface Script {
@@ -30,6 +68,7 @@ export interface Script {
     name?: string;
     description?: string;
     version?: string;
+    options?: Option[];
 
     load(context: ScriptContext): void | Promise<void>;
     unload(context: ScriptContext): void | Promise<void>;
